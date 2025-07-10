257fe1760df17dde7114be6676e95dfa1a04a43b
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-07-10 19:30:21 -0400

    Update world_generator.h

--- source/world_generator.h
+++ source/world_generator.h
@@ -2,17 +2,8 @@
 
 #include "map.h"
 #include <string>
-#include <nlohmann/json.hpp>
 
 class WorldGenerator {
 public:
-    WorldGenerator();
-    void Generate(Map* map, int width, int height);
-
-private:
-    nlohmann::json biomeData;
-
-    void loadBiomes(const std::string& path);
-    std::string pickBiome(float heightVal);
-    float PerlinNoise(float x, float y);
+	void Generate(Map* map, int width, int height);
 };
