bda2edf6d2fc2701fddb21a32658f5cbf20ac99f
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-07-10 21:06:34 -0400

    Create version.h

---
+++ source/version.h
@@ -0,0 +1,9 @@
+#ifndef RME_VERSION_H_
+#define RME_VERSION_H_
+
+#define RME_VERSION_STRING "3.5.0-dev"
+#define RME_VERSION_ID 350
+#define RME_VERSION_NAME "Enhanced Map Editor"
+#define RME_VERSION_BUILD 1
+
+#endif // RME_VERSION_H_
